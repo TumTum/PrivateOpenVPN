ec7a78faf667b24707710daec69e3d30b42e0ebd
Xiaopeng Qiu <qxp2004@gmail.com>
2023-09-24 03:27:46 +0200

    Anpassung für Ubuntu 22.04

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 VPN Server
 ----------
 
-Eigen verwalteter Server.
+Eigen verwalteter Server in Ubuntu 22.04.
 
 nach Anleitung von:
 
-https://www.digitalocean.com/community/tutorials/how-to-set-up-an-ikev2-vpn-server-with-strongswan-on-ubuntu-18-04-2
+https://www.digitalocean.com/community/tutorials/how-to-set-up-an-ikev2-vpn-server-with-strongswan-on-ubuntu-22-04
 
 
 1. IP und Domain anpassen:
--- files/VPNCert.sh
+++ files/VPNCert.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-find ~/pki/ -name "*.pem" -delete \
-&& find /etc/ipsec.d/ -name "*.pem" -delete \
-&& ipsec pki --gen --type rsa --size 1024 --outform pem > ~/pki/private/ca-key.pem \
-&& ipsec pki --self --ca --lifetime 3650 --in ~/pki/private/ca-key.pem --type rsa --dn "CN=VPN Freiheit tm2020" --outform pem > ~/pki/cacerts/ca-cert.pem \
-&& ipsec pki --gen --type rsa --size 1024 --outform pem > ~/pki/private/server-key.pem \
-&& ipsec pki --pub --in ~/pki/private/server-key.pem --type rsa \
-         | ipsec pki --issue --lifetime 1825 --cacert ~/pki/cacerts/ca-cert.pem  --cakey ~/pki/private/ca-key.pem \
-                     --dn "CN=amy.tmine.de" --san "amy.tmine.de" \
-                     --flag serverAuth --flag ikeIntermediate --outform pem  >  ~/pki/certs/server-cert.pem \
-&& cp -rfv ~/pki/* /etc/ipsec.d/ \
-&& cat /etc/ipsec.d/cacerts/ca-cert.pem > /var/www/html/freiheit.pem \
-&& systemctl restart strongswan
+find /etc/ipsec.d/ -name "*.pem" -delete 2>/dev/null \
+&& pki --gen --type rsa --size 4096 --outform pem > ~/pki/private/ca-key.pem \
+&& pki --self --ca --lifetime 3650 --in ~/pki/private/ca-key.pem  --type rsa --dn "CN=$1" --outform pem > ~/pki/cacerts/ca-cert.pem \
+&& pki --gen --type rsa --size 4096 --outform pem > ~/pki/private/server-key.pem \
+&& pki --pub --in ~/pki/private/server-key.pem --type rsa \
+    | pki --issue --lifetime 1825 \
+        --cacert ~/pki/cacerts/ca-cert.pem \
+        --cakey ~/pki/private/ca-key.pem \
+        --dn "CN=$1" --san $1 \
+        --flag serverAuth --flag ikeIntermediate --outform pem \
+    >  ~/pki/certs/server-cert.pem \
+&& cp -rfv ~/pki/* /etc/ipsec.d/
